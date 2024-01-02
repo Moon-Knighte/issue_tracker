547905fbe31a32ad51c623eb5cb4ed67577c1ff0
Moon-Knighte <githubanjan2255@gmail.com>
2024-01-02 21:04:22 +0545

    Extract the error message component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import { Text } from '@radix-ui/themes';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -44,14 +45,18 @@ const NewIssuePage = () => {
         <TextField.Root>
             <TextField.Input placeholder='Title' {...register('title')}/>
         </TextField.Root>
-        { errors.title && <Text color= 'red' as='p'>{ errors.title.message}</Text>}
+        <ErrorMessage>
+          { errors.title?.message}
+          </ErrorMessage>
         <Controller
         name='description'
         control={control}
         render={( field ) => <SimpleMDE 
           placeholder='Description' {...field}/> }
         />
-       { errors.description && <Text color= 'red' as='p'>{ errors.description.message}</Text>}
+      
+       <ErrorMessage>
+        { errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
     </form></div>
   )
